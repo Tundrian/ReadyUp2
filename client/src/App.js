5b18875b339057921d39bf65432b00884937aa01
Tundrian <antoniosansotta@gmail.com>
2022-06-09 19:16:06 -0400

    began adding a method to update platform tags on load to show proper colors

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { getGames, reset } from './features/library/librarySlice'
-import {useSelector, useDispatch} from 'react-redux'
+
 // Pages
 import NavBar from './components/navigation/NavBar'
 import NavMenu from "./components/navigation/NavMenu"
@@ -16,14 +15,11 @@ function App() {
  
   // Variables
   const [showNavMenu, setShowNavMenu] = useState(true)
-  const {games, isLoading, isError, isSuccess, message} = useSelector( (state) => state.library)
-  const dispatch = useDispatch()
 
   // Functions
   const toggleNavMenu = () => {
     setShowNavMenu(!showNavMenu)
   }
-  console.log(games)
 
   // Hooks
   useEffect(() => {
--- client/src/components/utilities/GameCard.js
+++ client/src/components/utilities/GameCard.js
@@ -5,65 +5,79 @@ import { setGame, getGames, deleteGame, reset } from '../../features/library/lib
 
 function GameCard({gameId, title, platforms, image}) {
   
+  const stylePlatforms = (platform) => {
+  
+    // get current element
+    // get platform that matches
+    // toggle classList property
+      let x = Array.prototype.slice.call(document.querySelectorAll('.gamecard-info-container')).filter(card => card.childNodes[0].innerText === title)
+      x = x.slice()[0]
+
+      console.log(x)
+      // .classList.toggle(`platform-${platform.toString().toLowerCase().replace(/[^A-Z0-9]+/ig, '')}`)
+  }
+  stylePlatforms()
+
   const dispatch = useDispatch()
-  const {games, isLoading, isError, isSuccess, message} = useSelector( (state) => state.library)
+  // const {games, isLoading, isError, isSuccess, message} = useSelector( (state) => state.library)
+  const games = useSelector((state) => state.library.games.filter(game => game.gameName === title))[0] || null
 
   const platformClick = async(e) => {
   
     // Variables
-    const element = e.target.classList
+    // const element = e.target.classList
     const user = JSON.parse(localStorage.getItem('user')) // stores user if logged in
     const gameSelected = e.target.parentElement.parentElement.childNodes[0].innerText // Get game name from card selected
     let removePlatform = false
     let newPlatforms
     let platformsToAdd = [e.target.textContent]// Get platform from card
-    let platformCase = ''
+    // let platformCase = ''
     let dbGames
-
-    switch( e.target.innerText){
-      case 'Nintendo Switch':
-       platformCase = 'nintendoSwitch'
-        break
-      case 'pc':
-        platformCase = 'PC'
-        break
-      case 'Xbox Series S/X':
-        platformCase = 'xboxSeriesSX'
-        break
-      case 'Xbox One':
-        platformCase = 'xboxOne'
-        break
-      case 'xbox360':
-        platformCase = 'xbox360'
-        break
-      case 'Playstation 3':
-        platformCase = 'playstation3'
-        break
-      case 'Playstation 4':
-        platformCase = 'playstation4'
-        break
-      case 'Playstation 5':
-        platformCase = 'playstation5'
-        break
-      case 'macOS':
-        platformCase = 'macOS'
-        break
-      case 'Linux':
-        platformCase = 'linux'
-        break
-      case 'PS Vita':
-        platformCase = 'psVita'
-        break
-      case 'Android':
-        platformCase = 'android'
-        break
-      case 'iOS':
-        platformCase = 'iOS'
-        break
-      default:
-        platformCase = 'PC'
-    }
-    element.toggle(platformCase)
+    stylePlatforms(e.target.innerText)
+    // switch( e.target.innerText){
+    //   case 'Nintendo Switch':
+    //    platformCase = 'nintendoSwitch'
+    //     break
+    //   case 'pc':
+    //     platformCase = 'PC'
+    //     break
+    //   case 'Xbox Series S/X':
+    //     platformCase = 'xboxSeriesSX'
+    //     break
+    //   case 'Xbox One':
+    //     platformCase = 'xboxOne'
+    //     break
+    //   case 'xbox360':
+    //     platformCase = 'xbox360'
+    //     break
+    //   case 'Playstation 3':
+    //     platformCase = 'playstation3'
+    //     break
+    //   case 'Playstation 4':
+    //     platformCase = 'playstation4'
+    //     break
+    //   case 'Playstation 5':
+    //     platformCase = 'playstation5'
+    //     break
+    //   case 'macOS':
+    //     platformCase = 'macOS'
+    //     break
+    //   case 'Linux':
+    //     platformCase = 'linux'
+    //     break
+    //   case 'PS Vita':
+    //     platformCase = 'psVita'
+    //     break
+    //   case 'Android':
+    //     platformCase = 'android'
+    //     break
+    //   case 'iOS':
+    //     platformCase = 'iOS'
+    //     break
+    //   default:
+    //     platformCase = 'PC'
+    // }
+    // element.toggle(platformCase)
 
     // Get game from database
     await (dispatch(getGames())
@@ -94,13 +108,20 @@ function GameCard({gameId, title, platforms, image}) {
       
 
       if(data.platforms.length === 0){
+
         toast.error(`${data.gameName} removed from library`, {theme: "dark"})
         dispatch(deleteGame(data))
+
       }else{
+
         if(removePlatform){
+
           toast.warning(`${platformsToAdd[0]} removed from ${data.gameName}`, {theme: "dark"})
+        
         }else{
+
           toast.success(`${platformsToAdd[0]} add to ${data.gameName}`, {theme: "dark"})
+        
         }
         
         dispatch(setGame(data))
@@ -122,7 +143,7 @@ function GameCard({gameId, title, platforms, image}) {
                     <li 
                     onClick={platformClick} 
                     key={platform.platform.name}
-                    className="gamecard-platform" 
+                    className="gamecard-platform"
                     >{platform.platform.name}</li>
                 ))}
                 </ul>
