ea144bc3c2e0fa0394eead0f1c3d608f8781746a
Antonio <antoniosansotta@gmail.com>
2022-07-01 10:04:04 -0400

    updating latest checkpoint

--- client/src/components/utilities/GameModal.js
+++ client/src/components/utilities/GameModal.js
@@ -31,9 +31,54 @@ function GameModal({open, children, onClose, game}) {
     <div style={OVERLAY_STYLES}></div>
     <div style={MODAL_STYLES} className="modal-container">
       <button className="modal-close-btn" onClick={onClose}>X</button>
-      <img className="modal-image" src={game.game.background_image} alt="game image" />
-      <div className="modal-info">
-        <h2>{game.game.name}</h2>
+      <div className="modal-content">
+        <img className="modal-image" src={game.game.background_image} alt="game image" />
+        <div className="modal-info">
+          <h2>{game.game.name}</h2>
+          <p>ESRB Rating: {game.game.esrb_rating.name}</p>
+          
+          <p>Rating: {game.game.rating}</p>
+          <p>Released: {game.game.released}</p>
+        </div>
+      <div className="modal-info-lists">
+          <div>
+            <h4>Genres</h4>
+            <ul>
+              {game.game.genres.map(g => (
+                <li className="modal-genres">{g.name}</li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h4>Platforms</h4>
+            <ul>
+              {game.game.platforms.map(p => (
+                <li>
+                  <h6>{p.platform.name}</h6>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+      
+      
+      <div>
+        <ul>
+          {game.game.short_screenshots.map(s => (
+            <img src={s.image}></img>
+          ))}
+        </ul>
+
+        <ul>
+          {game.game.stores.map(s => (
+            <li>
+              <p>{s.store.name}</p>
+              {/* <img src={s.store.image_background} alt="" /> */}
+            </li>
+          ))}
+        </ul>
+
       </div>
       {children}{children}
     </div>
