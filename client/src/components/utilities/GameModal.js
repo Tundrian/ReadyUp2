9e72d83ac4994cbc68e2a32b866429cbf53f19b1
Antonio <antoniosansotta@gmail.com>
2022-07-01 17:16:53 -0400

    Restarted layout changes for game detail card

--- client/src/components/utilities/GameModal.js
+++ client/src/components/utilities/GameModal.js
@@ -6,7 +6,7 @@ const MODAL_STYLES = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   // backgroundColor: '#FFF',
-  padding: '50px',
+  // padding: '50px',
   zIndex: 1000
 }
 
@@ -29,41 +29,71 @@ function GameModal({open, children, onClose, game}) {
   return ReactDom.createPortal(
     <>
     <div style={OVERLAY_STYLES}></div>
+
     <div style={MODAL_STYLES} className="modal-container">
       <button className="modal-close-btn" onClick={onClose}>X</button>
-      <div className="modal-content">
-        <img className="modal-image" src={game.game.background_image} alt="game image" />
-        <div className="modal-info">
-          <h2>{game.game.name}</h2>
-          <p>ESRB Rating: {game.game.esrb_rating.name}</p>
-          
-          <p>Rating: {game.game.rating}</p>
-          <p>Released: {game.game.released}</p>
+    
+      <div className="modal-banner-container">
+        <div className="modal-banner-image">
+          <img className="modal-image" src={game.game.background_image} alt="game image" />
+
         </div>
-      <div className="modal-info-lists">
-          <div>
-            <h4>Genres</h4>
-            <ul>
-              {game.game.genres.map(g => (
-                <li className="modal-genres">{g.name}</li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <h4>Platforms</h4>
-            <ul>
-              {game.game.platforms.map(p => (
-                <li>
-                  <h6>{p.platform.name}</h6>
-                </li>
-              ))}
-            </ul>
+        <div className="modal-banner-info">
+
+          <div className="modal-banner-info-platforms">
+
           </div>
         </div>
       </div>
-      
-      
-      <div>
+      {/* 
+        image spreading accross entire width
+
+        set text overtop of image using absolute positioning
+          - title
+          - release date
+          - ESRB rating and score rating - change to image
+          - description
+          - horizontal list of platforms - change to images
+        
+          under image section
+            - horizontal scroll of images
+            - show horizontal list of stores
+      */}
+
+      {/* <div className="modal-content">
+    
+        <img className="modal-image" src={game.game.background_image} alt="game image" />
+    
+        <div className="modal-info">
+    
+          <h2>{game.game.name}</h2>
+          <p>ESRB Rating: {game.game.esrb_rating.name}</p>
+          <p>Rating: {game.game.rating}</p>
+          <p>Released: {game.game.released}</p>
+
+          <div className="modal-info-lists">
+            <div>
+              <h4>Genres</h4>
+              <ul>
+                {game.game.genres.map(g => (
+                  <li className="modal-genres">{g.name}</li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h4>Platforms</h4>
+              <ul>
+                {game.game.platforms.map(p => (
+                  <li>
+                    <button className="btn modal-platform-btn">{p.platform.name}</button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div> */}
+      {/* <div>
         <ul>
           {game.game.short_screenshots.map(s => (
             <img src={s.image}></img>
@@ -75,14 +105,12 @@ function GameModal({open, children, onClose, game}) {
             <li>
               <p>{s.store.name}</p>
               {/* <img src={s.store.image_background} alt="" /> */}
-            </li>
+            {/* </li>
           ))}
         </ul>
 
-      </div>
-      {children}{children}
+      </div> */}
     </div>
-  
     </>,
     document.getElementById('portal')
     )
